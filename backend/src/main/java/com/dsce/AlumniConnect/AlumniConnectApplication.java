9eca2bdc6565cad86911795db27632e9412dfa8a
Anshul <anshpclg4040@gmail.com>
2026-06-30 00:30:56 +0530

    changed credentials

--- backend/src/main/java/com/dsce/AlumniConnect/AlumniConnectApplication.java
+++ backend/src/main/java/com/dsce/AlumniConnect/AlumniConnectApplication.java
@@ -12,7 +12,7 @@ import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication(exclude = {SessionAutoConfiguration.class})
+@SpringBootApplication(exclude = { SessionAutoConfiguration.class })
 public class AlumniConnectApplication {
 
 	public static void main(String[] args) {
@@ -22,15 +22,14 @@ public class AlumniConnectApplication {
 	@Bean
 	CommandLineRunner init(UserRepository userRepository) {
 		return args -> {
-			String adminEmail = "admin@example.com";
+			String adminEmail = "admin@dsce.com";
 			if (userRepository.findByEmail(adminEmail).isEmpty()) {
 				User admin = new User(
 						"Super",
 						"Admin",
 						adminEmail,
-						new BCryptPasswordEncoder().encode("admin123"), // hashed password
-						User.Role.ADMIN
-				);
+						new BCryptPasswordEncoder().encode("admin@123"), // hashed password
+						User.Role.ADMIN);
 				userRepository.save(admin);
 				System.out.println("Admin user created!");
 			}
